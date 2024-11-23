6855f55df9215c4e7b8cd64d3564d24178e87472
Abhay prajapati <abhayparja90@gmail.com>
2024-11-23 21:31:18 +0530

    hhb

--- controller/User.controoler.js
+++ controller/User.controoler.js
@@ -1,6 +1,6 @@
 const asyncWrapper = require("../middleware/async");
 const User = require("../models/User.model");
-const Blog = require('../models/Blog.model')
+const Blog = require("../models/Blog.model");
 exports.featchUser = asyncWrapper(async (req, res) => {
   const user = await User.findOne({ _id: req.user._id }).exec();
   const { name, email, image, isImage } = user;
@@ -11,27 +11,27 @@ exports.featchUser = asyncWrapper(async (req, res) => {
     isImage,
   });
 });
-exports.featchUserBloge = asyncWrapper(async(req,res) =>{
-  const userId = req.user._id 
+exports.featchUserBloge = asyncWrapper(async (req, res) => {
+  const userId = req.user._id;
   const blogs = await Blog.find({ userId: userId });
-  res.status(201).json(blogs)
-})
+  res.status(201).json({ blogs });
+});
 exports.updateUser = asyncWrapper(async (req, res) => {
   const { image, name, email, bio, phone } = req.body;
   const updateObj = { image, name, email, bio, phone };
   if (image != "") updateObj.isImage = true;
   // console.log(updateObj)
 
-  const user = await User.findByIdAndUpdate(req.user._id, updateObj, { new: true }   );
-  console.log(user)
-  res
-    .status(200)
-    .json({
-      name: user.name,
-      image: user.image,
-      email: user.email,
-      phone: user.phone,
-      bio: user.bio,
-      isImage: user.isImage,
-    });
+  const user = await User.findByIdAndUpdate(req.user._id, updateObj, {
+    new: true,
+  });
+  console.log(user);
+  res.status(200).json({
+    name: user.name,
+    image: user.image,
+    email: user.email,
+    phone: user.phone,
+    bio: user.bio,
+    isImage: user.isImage,
+  });
 });
