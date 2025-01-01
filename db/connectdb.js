f1c1cf465ccefd04a4bec8ed65d800b8edd8b4dc
Abhay prajapati <abhayparja90@gmail.com>
2025-01-01 09:41:03 +0530

    some bug solve

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 async function connectDb() {
-<<<<<<< HEAD
+
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       connectTimeoutMS: 30000, // Increase timeout to 30 seconds
@@ -10,15 +10,7 @@ async function connectDb() {
   } catch (error) {
     console.log(error);
   }
-=======
-    try {
-   await mongoose.connect(process.env.MONGODB_URL,{ connectTimeoutMS: 30000, // Increase timeout to 30 seconds
-    socketTimeoutMS: 45000 });
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.log(error);
-    }
->>>>>>> d421006088cb46ced9a955411cf707e0b4d5192b
+
 }
 
 exports.connectDb = connectDb;
