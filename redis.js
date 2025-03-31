93e7788aa59cbd1b620136eb9ac7c7859b0ac10a
Abhay prajapati <140682257+abhay395@users.noreply.github.com>
2025-03-31 10:09:27 +0000

    commite redis

--- redis.js
+++ redis.js
@@ -1,20 +1,20 @@
-// redis.js
-const Redis = require('ioredis');
-const dotenv = require('dotenv')
-dotenv.config()
-// Configure Redis connection
-const redis = new Redis({
-  host: process.env.REDIS_HOST, // Redis server host
-  port: process.env.REDIS_PORT,        // Redis server port
-  password: process.env.REDIS_PASSWORD,      // Use password if required
-});
+// // redis.js
+// const Redis = require('ioredis');
+// const dotenv = require('dotenv')
+// dotenv.config()
+// // Configure Redis connection
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST, // Redis server host
+//   port: process.env.REDIS_PORT,        // Redis server port
+//   password: process.env.REDIS_PASSWORD,      // Use password if required
+// });
 
-redis.on('connect', () => {
-  console.log('Connected to Redis');
-});
+// redis.on('connect', () => {
+//   console.log('Connected to Redis');
+// });
 
-redis.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
+// redis.on('error', (err) => {
+//   console.error('Redis connection error:', err);
+// });
 
-module.exports = redis;
+// module.exports = redis;
